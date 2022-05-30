34f710819f1693c90a0c2c433da644b89f927b09
Yunbok <qodbsqhr@naver.com>
2022-05-30 22:56:38 +0900

    타임리프와 스프링 예제

---
+++ src/main/java/hello/itemservice/domain/item/ItemType.java
@@ -0,0 +1,16 @@
+package hello.itemservice.domain.item;
+
+public enum ItemType {
+
+    BOOK("책"), FOOD("음식"), ETC("기타");
+
+    private final String description;
+
+    ItemType(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
