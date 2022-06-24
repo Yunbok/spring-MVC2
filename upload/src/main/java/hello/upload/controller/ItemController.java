34ff7c066813ada3accb21c8d721c39a8baadf61
YUNBOK <qodbsqhr@naver.com>
2022-06-24 10:38:10 +0900

    item-view 안나오는현상 수정
    - 원인 : UploadFile에 @Data 누락

--- upload/src/main/java/hello/upload/controller/ItemController.java
+++ upload/src/main/java/hello/upload/controller/ItemController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.io.IOException;
@@ -50,9 +49,8 @@ public class ItemController {
     @GetMapping("/items/{id}")
     public String items(@PathVariable Long id, Model model) {
         Item item = itemRepository.findById(id);
-
-        log.info("item = {}", item);
         model.addAttribute("item", item);
         return "item-view";
     }
+
 }
